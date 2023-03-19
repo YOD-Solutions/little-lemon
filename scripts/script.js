7fcfc21bd73420c5c7fee1b36657cf9370e4a204
YOD-Solutions <baya.dawdo@gmail.com>
2023-03-20 09:54:51 +1100

    more edits

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+const menu_section = document.querySelector('#menu');
+
+const options={
+    root:null,
+    rootMargin:'0px',
+    threshold:0.5
+}
+
+const observer = new IntersectionObserver(enteries => {
+    enteries.forEach(entry =>{
+        if(entry.isIntersecting){
+            menu_section.classList.add("animate");
+            menu_section.style.animation = 'slide-in 2s ease-out';    
+        } else{
+            menu_section.classList.remove("animate");
+            menu_section.style.animation = '';  
+        }
+    });
+},options);
+
+observer.observe(menu_section);
