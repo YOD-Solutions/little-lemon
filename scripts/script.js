4ede415cecacf654d44530e65e5d580ecb03f1d6
YOD-Solutions <baya.dawdo@gmail.com>
2023-03-23 13:06:46 +1100

    more stuff add

--- scripts/script.js
+++ scripts/script.js
@@ -15,10 +15,10 @@ const observer = new IntersectionObserver(enteries => {
         if(entry.isIntersecting){        
         
             if(entry.target.nodeName == "SECTION"){
-                entry.target.style.visibility="visible";                    
+                entry.target.style['animation-name']='fadeInUp';                 
                 entry.target.style['animation-duration']='1000ms'; 
-                entry.target.style['animation-delay']='300ms'; 
-                entry.target.style['animation-name']='fadeInUp';                  
+                entry.target.style['animation-delay']='300ms';                 
+                entry.target.style.visibility="visible";                     
                 entry.target.classList.add("animated");
                     
             }
